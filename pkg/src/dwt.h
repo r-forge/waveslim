7a346bbd1e638ea7f1ac6063df4e68a67c7901f7
bwhitcher (Brandon Whitcher) <bwhitcher@gmail.com>
2009-12-07 17:36:24 +0000

    Added dwt.h for two-d and three-d wavelet transforms
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/waveslim@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/dwt.h
@@ -0,0 +1,13 @@
+/*************************************************************************/
+void dwt(double *Vin, int *M, int *L, double *h, double *g, 
+	 double *Wout, double *Vout)
+/*************************************************************************/
+void idwt(double *Win, double *Vin, int *M, int *L, double *h, double *g, 
+	  double *Xout)
+/*************************************************************************/
+void modwt(double *Vin, int *N, int *j, int *L, double *ht, double *gt, 
+	   double *Wout, double *Vout)
+/*************************************************************************/
+void imodwt(double *Win, double *Vin, int *N, int *j, int *L, 
+	    double *ht, double *gt, double *Vout)
+/*************************************************************************/
