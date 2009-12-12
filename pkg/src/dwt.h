2f82567045a225ab094d2b864b80c455fe5fb712
bwhitcher (Brandon Whitcher) <bwhitcher@gmail.com>
2009-12-12 12:00:33 +0000

    Added ";" to the end of each prototype
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/waveslim@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/dwt.h
+++ pkg/src/dwt.h
@@ -1,13 +1,12 @@
-/*************************************************************************/
-void dwt(double *Vin, int *M, int *L, double *h, double *g, 
-	 double *Wout, double *Vout)
-/*************************************************************************/
-void idwt(double *Win, double *Vin, int *M, int *L, double *h, double *g, 
-	  double *Xout)
-/*************************************************************************/
-void modwt(double *Vin, int *N, int *j, int *L, double *ht, double *gt, 
-	   double *Wout, double *Vout)
-/*************************************************************************/
-void imodwt(double *Win, double *Vin, int *N, int *j, int *L, 
-	    double *ht, double *gt, double *Vout)
-/*************************************************************************/
+extern void dwt(double *Vin, int *M, int *L, double *h, double *g, 
+	          double *Wout, double *Vout);
+
+extern void idwt(double *Win, double *Vin, int *M, int *L, double *h, double *g, 
+	           double *Xout);
+
+extern void modwt(double *Vin, int *N, int *j, int *L, double *ht, double *gt, 
+	            double *Wout, double *Vout);
+
+extern void imodwt(double *Win, double *Vin, int *N, int *j, int *L, 
+	             double *ht, double *gt, double *Vout);
+
